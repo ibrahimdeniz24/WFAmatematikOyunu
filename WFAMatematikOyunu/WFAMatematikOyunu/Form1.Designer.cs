f00421cd88f2e3e52e4463c49370901a142818b6
ibrahimdeniz24 <160463714+ibrahimdeniz24@users.noreply.github.com>
2024-02-19 17:18:27 +0300

    matematikOyunu
    Matematik Oyunu

---
+++ WFAMatematikOyunu/WFAMatematikOyunu/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace WFAMatematikOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblsayi1 = new System.Windows.Forms.Label();
+            this.lblOperator = new System.Windows.Forms.Label();
+            this.lblSayi2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtgirilenDeger = new System.Windows.Forms.TextBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lblSoruSayisi = new System.Windows.Forms.Label();
+            this.lblDogruCevapSayisi = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblsayi1
+            // 
+            this.lblsayi1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblsayi1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblsayi1.Location = new System.Drawing.Point(42, 117);
+            this.lblsayi1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblsayi1.Name = "lblsayi1";
+            this.lblsayi1.Size = new System.Drawing.Size(161, 119);
+            this.lblsayi1.TabIndex = 0;
+            // 
+            // lblOperator
+            // 
+            this.lblOperator.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblOperator.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblOperator.Location = new System.Drawing.Point(247, 117);
+            this.lblOperator.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblOperator.Name = "lblOperator";
+            this.lblOperator.Size = new System.Drawing.Size(161, 119);
+            this.lblOperator.TabIndex = 0;
+            // 
+            // lblSayi2
+            // 
+            this.lblSayi2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblSayi2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSayi2.Location = new System.Drawing.Point(466, 117);
+            this.lblSayi2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblSayi2.Name = "lblSayi2";
+            this.lblSayi2.Size = new System.Drawing.Size(161, 119);
+            this.lblSayi2.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(674, 117);
+            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(161, 119);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "=";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtgirilenDeger
+            // 
+            this.txtgirilenDeger.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtgirilenDeger.Location = new System.Drawing.Point(908, 117);
+            this.txtgirilenDeger.Margin = new System.Windows.Forms.Padding(5);
+            this.txtgirilenDeger.Multiline = true;
+            this.txtgirilenDeger.Name = "txtgirilenDeger";
+            this.txtgirilenDeger.Size = new System.Drawing.Size(247, 119);
+            this.txtgirilenDeger.TabIndex = 1;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(1233, 117);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(143, 119);
+            this.btnOk.TabIndex = 2;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(79, 79);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 25);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Sayı - 1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(277, 79);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(97, 25);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Operatör";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(500, 79);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(87, 25);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "Sayı - 2";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(54, 313);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(142, 25);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "Soru Sayısı : ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(422, 313);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(217, 25);
+            this.label9.TabIndex = 3;
+            this.label9.Text = "Doğru Cevap Sayısı :";
+            // 
+            // lblSoruSayisi
+            // 
+            this.lblSoruSayisi.AutoSize = true;
+            this.lblSoruSayisi.Location = new System.Drawing.Point(202, 313);
+            this.lblSoruSayisi.Name = "lblSoruSayisi";
+            this.lblSoruSayisi.Size = new System.Drawing.Size(24, 25);
+            this.lblSoruSayisi.TabIndex = 3;
+            this.lblSoruSayisi.Text = "0";
+            // 
+            // lblDogruCevapSayisi
+            // 
+            this.lblDogruCevapSayisi.AutoSize = true;
+            this.lblDogruCevapSayisi.Location = new System.Drawing.Point(657, 313);
+            this.lblDogruCevapSayisi.Name = "lblDogruCevapSayisi";
+            this.lblDogruCevapSayisi.Size = new System.Drawing.Size(24, 25);
+            this.lblDogruCevapSayisi.TabIndex = 3;
+            this.lblDogruCevapSayisi.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1570, 703);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lblDogruCevapSayisi);
+            this.Controls.Add(this.lblSoruSayisi);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.txtgirilenDeger);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblSayi2);
+            this.Controls.Add(this.lblOperator);
+            this.Controls.Add(this.lblsayi1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblsayi1;
+        private System.Windows.Forms.Label lblOperator;
+        private System.Windows.Forms.Label lblSayi2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtgirilenDeger;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lblSoruSayisi;
+        private System.Windows.Forms.Label lblDogruCevapSayisi;
+    }
+}
+
---
+++ WFAMatematikOyunu/WFAMatematikOyunu/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFAMatematikOyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rnd = new Random();
+        int sayi1, sayi2, op, soruSayisi, dogruCevapSayisi, sonuc;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            sayi1 = rnd.Next(1, 11);
+            sayi2 = rnd.Next(1, 11);
+
+            lblsayi1.Text = sayi1.ToString();
+            lblSayi2.Text = sayi2.ToString();
+
+            op = rnd.Next(1, 3);
+
+            if (op == 1)
+            {
+                lblOperator.Text = "+";
+            }
+
+            else
+            {
+                lblOperator.Text = "-"; 
+            }
+
+            soruSayisi++;
+
+            lblSoruSayisi.Text=soruSayisi.ToString(); 
+
+             
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int girilenDeger = Convert.ToInt32(txtgirilenDeger.Text);
+
+                if (op == 1)
+
+                {
+                    sonuc = sayi1 + sayi2;
+
+                }
+                else
+
+                {
+                    sonuc = sayi1 - sayi2;
+                }
+
+                if (girilenDeger == sonuc)
+
+                {
+                    dogruCevapSayisi++;
+
+                    lblDogruCevapSayisi.Text = dogruCevapSayisi.ToString();
+
+                }
+
+
+                else
+                {
+                    soruSayisi++;
+                    lblSoruSayisi.Text = soruSayisi.ToString();
+
+                }
+
+                {
+                    sayi1 = rnd.Next(1, 11);
+                    sayi2 = rnd.Next(1, 11);
+
+                    lblsayi1.Text = sayi1.ToString();
+                    lblSayi2.Text = sayi2.ToString();
+
+                    op = rnd.Next(1, 3);
+
+                    if (op == 1)
+                    {
+                        lblOperator.Text = "+";
+                    }
+
+                    else
+                    {
+                        lblOperator.Text = "-";
+                    }
+
+                    soruSayisi++;
+
+                    lblSoruSayisi.Text = soruSayisi.ToString();
+                }
+            }
+
+            catch
+
+            {
+                MessageBox.Show(" Lütfen Sayısal bir değer giriniz !!");
+            }
+
+        }
+    }
+}
